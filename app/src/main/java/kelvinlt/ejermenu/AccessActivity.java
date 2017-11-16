4a004587ab848ce4928420e31e0b35c376a4c567
kelvinlt <kelvinleethen@gmail.com>
2017-11-16 18:28:28 +0100

    ...

--- app/src/main/java/kelvinlt/ejermenu/AccessActivity.java
+++ app/src/main/java/kelvinlt/ejermenu/AccessActivity.java
@@ -1,6 +1,9 @@
 package kelvinlt.ejermenu;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,8 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
 
 /**
  * Created by alu2012095 on 19/10/2017.
@@ -17,8 +23,10 @@ import org.w3c.dom.Text;
 
 public class AccessActivity extends AppCompatActivity {
     private ImageView imgVista;
-    private ImageButton takeFoto , searchFoto;
+    private ImageButton takeFoto, searchFoto;
     private String userName;
+    private Intent data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +40,74 @@ public class AccessActivity extends AppCompatActivity {
         takeFoto = (ImageButton) findViewById(R.id.take_foto);
         searchFoto = (ImageButton) findViewById(R.id.search_foto);
 
-        editUsername.setOnClickListener(new View.OnClickListener(){
+        searchFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(editUsername.getText().equals("Insert your username"))
-                editUsername.setText("");
+                selectPhoto();
+            }
+        });
+
+        takeFoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                takePhoto();
             }
         });
 
     }
+    @Override
+     public void onPause() {
+        emptyImageViewer();
+        super.onPause();
+    }
+//
+    public void emptyImageViewer() {
+         Drawable drawable = imgVista.getDrawable();
+         if (drawable != null) {
+             ((BitmapDrawable)drawable).getBitmap().recycle();
+             }
+        imgVista.setImageURI(null);
+         }
+
+    private final static int SELECT_PHOTO = 1;
+    private final static int CAPTURE_PHOTO = 2;
+    private Uri photoUri;
+
+    public void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(
+                Intent.createChooser(intent, "Select␣Source"), SELECT_PHOTO
+        );
+    }
+
+    public void takePhoto() {
+        File dir = getExternalFilesDir(null);
+        if (dir == null) {
+            Toast.makeText(this, "Unable␣to␣mount␣the␣filesystem",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        File file = new File(dir, "photo.jpg");
+        photoUri = Uri.fromFile(file);
+        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(i, CAPTURE_PHOTO);
+    }
+    @Override
+     protected void onActivityResult(int requestCode , int resultCode, Intent data) {
+         super.onActivityResult(requestCode , resultCode, data);
+//         if (resultCode != RESULT_OK) return;
+//         emptyImageViewer();
+         if (requestCode == CAPTURE_PHOTO) {
+             imgVista.setImageURI(photoUri);
+             }
+         else {
+             Uri img = data.getData();
+             if (img != null) {
+                 imgVista.setImageURI(img);
+                 }
+             }
+        imgVista.invalidate();
+         }
 }
---
+++ app/src/main/java/kelvinlt/ejermenu/AjustesActivity.java
@@ -0,0 +1,11 @@
+package kelvinlt.ejermenu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alu2012095 on 23/10/2017.
+ */
+
+public class AjustesActivity extends AppCompatActivity {
+
+}
